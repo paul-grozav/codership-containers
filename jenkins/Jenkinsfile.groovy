dc2a45c0e394e0a5cc1d9b34e082539347b918bc
Alexey Bychko <abychko@gmail.com>
2024-05-20 10:58:19 +0700

    Testing for codership/docker repository PRs #546
     added simple template for jenkins job

---
+++ jenkins/Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+#!groovy
+
+pipeline {
+    agent { label 'docker' }
+
+    environment {
+        image = "codership/mysql-galera-test"
+        RH_VERSION = "9"
+        TAG="develop"
+        DOCKERHUBCREDS = credentials('DockerHub')
+    }
+
+    stages {
+        stage('Docker Build') {
+
+              steps {
+                sh '''
+                    cd mysql-galera/image
+                    docker build \
+                        --build-arg RH_VERSION=${RH_VERSION} \
+                        --build-arg MYSQL_RPM_VERSION=${MYSQL_RPM_VERSION} \
+                        -t $image:${TAG} -f Dockerfile .
+                    '''
+                }
+
+        }
+        stage('Docker Push') {
+            steps {
+                sh '''
+
+                    docker login --username ${DOCKERHUBCREDS_USR} --password ${DOCKERHUBCREDS_PSW}
+                    docker push $image:${TAG}
+                   '''
+            }
+        }
+        stage('Cleanup') {
+            steps {
+                sh 'docker rmi -f $image:${TAG}'
+            }
+        }
+    }
+}
